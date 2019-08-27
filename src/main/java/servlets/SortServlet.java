8c22627aef0a9942365781f9be36e6ecda0df3dc
Vladimir Chamkin <vovacham@mail.ru>
2019-08-27 10:26:38 +0300

    added random sorting

--- src/main/java/servlets/SortServlet.java
+++ src/main/java/servlets/SortServlet.java
@@ -20,26 +20,41 @@ public class SortServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String mass = req.getParameter("mass").trim();
-        String[] arrayStr = mass.split(" ");
-        try {
-            int[] arrayInt = new int[arrayStr.length];
-            for (int i = 0; i < arrayInt.length; i++) {
-                arrayInt[i] = Integer.parseInt(arrayStr[i]);
+        String action = req.getParameter("action");
+
+        if (action.equals("sort")) {
+            String mass = req.getParameter("mass").trim();
+            String[] arrayStr = mass.split(" ");
+            try {
+                int[] arrayInt = new int[arrayStr.length];
+                for (int i = 0; i < arrayInt.length; i++) {
+                    arrayInt[i] = Integer.parseInt(arrayStr[i]);
+                }
+
+                req.setAttribute("message", "Массив: " + mass);
+                req.setAttribute("sortArray", "Отсортированный массив: " + arrayToString(Test1.sortArray(arrayInt)));
+                doGet(req, resp);
+            } catch (Exception e) {
+                req.setAttribute("message", "Некорректный ввод данных, повторите ввод");
+                req.setAttribute("sortArray", mass);
+                doGet(req, resp);
             }
+        }
 
-            String result = Arrays.toString(Test1.sortArray(arrayInt));
-            result = result.substring(1, result.length() - 1);
-            result = result.replaceAll(",", "");
+        if (action.equals("random")) {
+            int[] arrayInt = Test1.newArray(20, 100);
+            int[] arraySort = arrayInt.clone();
 
-            req.setAttribute("message", "Отсортированный массив");
-            req.setAttribute("sortArray", result);
-            doGet(req, resp);
-        } catch (Exception e) {
-            req.setAttribute("message", "Некорректный ввод данных, повторите ввод");
-            req.setAttribute("sortArray", mass);
+            req.setAttribute("message", "Случайный массив: " + arrayToString(arrayInt));
+            req.setAttribute("sortArray", "Отсортированный массив: " + arrayToString(Test1.sortArray(arraySort)));
             doGet(req, resp);
         }
+    }
+
+    private static String arrayToString(int[] array) {
+        String s = Arrays.toString(array);
+        String result = s.substring(1, s.length() - 1).replaceAll(",", "");
+        return result;
     }
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -7,30 +7,37 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
+<head>
     <title>Сортировка массива</title>
     <link rel="stylesheet" href="styles/w3.css">
-  </head>
+</head>
 
 
-  <body>
-  <div class="w3-container w3-center">
+<body>
+<div class="w3-container w3-center">
     <h2>Сортировка массива</h2>
-  </div>
-  <form method="post" class="w3-selection w3-light-grey w3-padding">
+</div>
+<form method="post" class="w3-selection w3-light-grey w3-padding">
     <h2>Введите элементы массива через пробел</h2>
     <label>
-      <input type="text" name="mass" class="w3-input w3-animate-input w3-border w3-round-large" style="width: 30%"><br />
+        <input type="text" name="mass" class="w3-input w3-animate-input w3-border w3-round-large"
+               style="width: 30%"><br/>
     </label>
-    <button type="submit" class="w3-btn w3-green w3-round-large w3-margin-bottom">Сортировка</button>
-  </form>
+    <button type="submit" name="action" value="sort" class="w3-btn w3-green w3-round-large w3-margin-bottom">
+        Сортировка
+    </button>
+    <button type="submit" name="action" value="random" class="w3-btn w3-green w3-round-large w3-margin-bottom">Случайный
+        массив
+    </button>
+</form>
 
-  <%
+
+<%
     if (request.getAttribute("sortArray") != null) {
-      out.print("<div class=\"w3-container\" <p><font size=5>"+ request.getAttribute("message") + "</font></p>\n" + "</div>");
-      out.print("<div class=\"w3-container\" <h2>" + request.getAttribute("sortArray") + "</h2>\n" + "</div>");
+        out.print("<div class=\"w3-container\" <p><font size=4>" + request.getAttribute("message") + "</font></p>\n" + "</div>");
+        out.print("<div class=\"w3-container\" <h2>" + request.getAttribute("sortArray") + "</h2>\n" + "</div>");
     }
-  %>
+%>
 
-  </body>
+</body>
 </html>
