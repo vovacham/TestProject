e8ec3591075c3b9b6ad3d2f056b60a9420f55e9e
Vladimir Chamkin <vovacham@mail.ru>
2019-09-05 13:22:50 +0300

    added id

--- src/main/java/model/Services.java
+++ src/main/java/model/Services.java
@@ -6,20 +6,26 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Services {
 
-    //Хранилище данных
+    /** Хранилище данных */
     private static Map<Integer, String[]> mapId = new ConcurrentHashMap<>();
 
-    //Возврат значения элемента из mapId
+    /**
+     * Возврат значения элемента из mapId
+     */
     public static String[] getValueFromMapId(int id) {
         return mapId.get(id);
     }
 
-    //Добавление элемента в mapId
+    /**
+     * Добавление элемента в mapId
+     */
     public static void addMapId(Integer id, String[] array) {
         mapId.put(id, array);
     }
 
-    //Генерация случайного массива
+    /**
+     * Генерация случайного массива
+     */
     public static int[] newRandomArray(int num, int max) {
         int[] array = new int[num];
         for (int i = 0; i < num; i++) {
@@ -28,14 +34,18 @@ public class Services {
         return array;
     }
 
-    //Возврат отсортированной копии массива
+    /**
+     * Возврат отсортированной копии массива
+     */
     public static int[] sortArray(int[] array) {
         int[] result = array.clone();
         QuickSort.sort(result, 0, array.length - 1);
         return result;
     }
 
-    //Строчное отображение массива без фигурных скобок
+    /**
+     * Строчное отображение массива без фигурных скобок
+     */
     public static String arrayToString(int[] array) {
         String s = Arrays.toString(array);
         String result = s.substring(1, s.length() - 1); //.replaceAll(",", "");
